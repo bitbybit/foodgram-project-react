73db27dfbf5f4329f22e12b4e1868993d9e3c85a
Михаил <1628191+bitbybit@users.noreply.github.com>
2022-08-04 23:33:14 +0600

    model user

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,31 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    ROLE_CHOICES = (
+        ("user", "пользователь"),
+        ("admin", "администратор"),
+    )
+
+    role = models.CharField(
+        max_length=25,
+        verbose_name="Роль пользователя",
+        choices=ROLE_CHOICES,
+        default="user",
+    )
+    password = models.CharField(
+        verbose_name="Пароль",
+        max_length=150,
+    )
+    first_name = models.CharField(
+        verbose_name="Имя",
+        max_length=150,
+    )
+    last_name = models.CharField(
+        verbose_name="Фамилия",
+        max_length=150,
+    )
+    email = models.EmailField(
+        verbose_name="Email",
+    )
