665bafb8fd3bf512ac6c0476e4e1fb0e954c38c5
Михаил <1628191+bitbybit@users.noreply.github.com>
2022-08-07 22:22:50 +0600

    users urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,6 @@
 from django.urls import include, path
-from rest_framework.routers import SimpleRouter
-
-router = SimpleRouter()
 
 urlpatterns = [
-    path("auth/", include("djoser.urls.authtoken")),
-    path("", include("djoser.urls")),
-    path("", include(router.urls)),
+    path("", include("users.urls")),
+    path("", include("food.urls")),
 ]
