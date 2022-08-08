5087a5ea2fa367cdf276a62a829d08936e4e2cbe
Михаил <1628191+bitbybit@users.noreply.github.com>
2022-08-08 16:00:30 +0600

    recipes serializers

--- backend/food/serializers.py
+++ backend/food/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from users.serializers import UserSerializer
 
-from .models import Tag, Ingredient
+from .models import Ingredient, IngredientInRecipe, Recipe, Tag
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -24,3 +25,70 @@ class IngredientSerializer(serializers.ModelSerializer):
         )
 
         model = Ingredient
+
+
+class IngredientInRecipeSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source="ingredient.id")
+    measurement_unit = serializers.ReadOnlyField(
+        source="ingredient.measurement_unit"
+    )
+    name = serializers.ReadOnlyField(source="ingredient.name")
+
+    class Meta:
+        fields = (
+            "amount",
+            "id",
+            "measurement_unit",
+            "name",
+        )
+
+        model = IngredientInRecipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(
+        many=True,
+        required=True,
+        source="tag",
+    )
+    author = UserSerializer(
+        required=True,
+    )
+    ingredients = IngredientInRecipeSerializer(
+        many=True,
+        source="ingredientinrecipe_set",
+    )
+    is_favorited = serializers.SerializerMethodField()
+    is_in_shopping_cart = serializers.SerializerMethodField()
+
+    def get_is_favorited(self, obj: Recipe) -> bool:
+        user = self.context.get("request").user
+
+        if not user.is_authenticated:
+            return False
+
+        return user.favorite.filter(id=obj.id).exists()
+
+    def get_is_in_shopping_cart(self, obj: Recipe) -> bool:
+        user = self.context.get("request").user
+
+        if not user.is_authenticated:
+            return False
+
+        return user.cart.filter(id=obj.id).exists()
+
+    class Meta:
+        fields = (
+            "author",
+            "cooking_time",
+            "id",
+            "image",
+            "ingredients",
+            "is_favorited",
+            "is_in_shopping_cart",
+            "name",
+            "tags",
+            "text",
+        )
+
+        model = Recipe
