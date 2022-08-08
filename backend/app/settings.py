a44a3490541af18343e494ba6085bbd0840f96a0
Михаил <1628191+bitbybit@users.noreply.github.com>
2022-08-08 15:58:04 +0600

    env MEDIA_URL

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -89,7 +89,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = "/media/"
+MEDIA_URL = os.getenv("MEDIA_URL", "")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = "users.User"
