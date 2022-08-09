aaeb9c9b709fa11701f2006d36ce5aa2dd3a693e
Михаил <1628191+bitbybit@users.noreply.github.com>
2022-08-09 23:09:59 +0600

    recipes urls, viewset

--- backend/food/urls.py
+++ backend/food/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import IngredientViewSet, TagViewSet
+from .views import IngredientViewSet, RecipeViewSet, TagViewSet
 
 router = SimpleRouter()
 router.register("tags", TagViewSet)
 router.register("ingredients", IngredientViewSet)
+router.register("recipes", RecipeViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
--- backend/food/views.py
+++ backend/food/views.py
@@ -1,9 +1,12 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, viewsets
 
-from .filtersets import IngredientFilter
-from .models import Ingredient, Tag
-from .serializers import IngredientSerializer, TagSerializer
+from .filtersets import IngredientFilter, RecipeFilter
+from .models import Ingredient, Recipe, Tag
+from .pagination import PageNumberLimitPagination
+from .permissions import IsAdminOrAuthorOrReadOnly
+from .serializers import (IngredientSerializer, RecipeSerializer,
+                          RecipeSerializerWrite, TagSerializer)
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
@@ -20,3 +23,20 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     pagination_class = None
     filter_backends = (DjangoFilterBackend,)
     filterset_class = IngredientFilter
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    permission_classes = (IsAdminOrAuthorOrReadOnly,)
+    pagination_class = PageNumberLimitPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = RecipeFilter
+
+    def get_serializer_class(self):
+        if self.action in (
+            "create",
+            "partial_update",
+            "update",
+        ):
+            return RecipeSerializerWrite
+        return RecipeSerializer
